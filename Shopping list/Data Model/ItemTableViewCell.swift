d35a6043a71d4f2dd3c65ec99da32d70b02e1d3f
elina-mns <ellie.mansurova@gmail.com>
2021-01-07 15:07:23 -0500

    add custom cell

---
+++ Shopping list/Data Model/ItemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ItemTableViewCell.swift
+//  Shopping list
+//
+//  Created by Elina Mansurova on 2021-01-07.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
