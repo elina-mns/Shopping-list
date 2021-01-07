0d12746782a0b139668b4851ab79ce385c2b9fda
elina-mns <ellie.mansurova@gmail.com>
2021-01-07 15:43:11 -0500

    add protocol, outlets for the cell

--- Shopping list/Data Model/ItemTableViewCell.swift
+++ Shopping list/Data Model/ItemTableViewCell.swift
@@ -7,17 +7,36 @@
 
 import UIKit
 
+protocol ItemTableViewCellDelegate: class {
+    func didTapAddToBag(_ cell: UITableViewCell)
+}
+
 class ItemTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var iconImage: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var addToBag: UIButton!
+    
+    weak var delegate: ItemTableViewCellDelegate?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        backgroundColor = .clear
+    }
+    
+    @IBAction func didTapAddToBag(_ sender: UIButton) {
+        delegate?.didTapAddToBag(self)
+    }
+    
+    static let identifier = "ItemTableViewCell"
+    static func nib() -> UINib {
+        return UINib(nibName: "ItemTableViewCell",
+                     bundle: nil)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
 }
--- Shopping list/ViewControllers/ListTableViewController.swift
+++ Shopping list/ViewControllers/ListTableViewController.swift
@@ -9,30 +9,43 @@ import UIKit
 
 class ListTableViewController: UITableViewController {
     
-    private var items: [String] = []
+    private var items: [String] = (1...10).map { "Test\($0)" }
     private var item: String = "Test"
+    private var itemsInBasket: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableView.register(ItemTableViewCell.nib(), forCellReuseIdentifier: "ItemTableViewCell")
     }
 
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-        item = items[indexPath.row]
-        //cell.textLabel?.text = item.name
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ItemTableViewCell", for: indexPath) as? ItemTableViewCell else {
+            fatalError()
+        }
+        let item = items[indexPath.row]
+        cell.name.text = item
+        // cell.price.text = item.price
+        // cell.icon.image = item.image
+        cell.delegate = self
         return cell
     }
     
 
+}
+
+extension ListTableViewController: ItemTableViewCellDelegate {
+    func didTapAddToBag(_ cell: UITableViewCell) {
+        guard let indexPath = tableView.indexPath(for: cell) else {
+            return
+        }
+        let item = items[indexPath.row]
+        itemsInBasket.append(item)
+        title = "\(itemsInBasket.count) items"
+    }
 }
