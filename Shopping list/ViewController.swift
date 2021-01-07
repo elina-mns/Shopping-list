96fb052ac78f2b8bd26c534c6b3ff7dc0e14f251
elina-mns <ellie.mansurova@gmail.com>
2021-01-07 13:53:25 -0500

    Initial Commit

---
+++ Shopping list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shopping list
+//
+//  Created by Elina Mansurova on 2021-01-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
